8cf6cacf66894653289659eedb063465fca18f3a
Raphaël Grand <otumbarg@gmail.com>
2023-10-13 13:40:54 +0200

    chantier filter

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -22,6 +22,27 @@ document.addEventListener('DOMContentLoaded', async function () {
             figure.appendChild(figcaption);
             galleryDiv.appendChild(figure);
         });
+
+        // filtre 
+
+            const categoriesResponse = await fetch('http://localhost:5678/api/categories');
+
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+
+            const categories = await categoriesResponse.json();
+            const filterButtons = document.getElementById('filter-buttons');
+
+            for (const categorie of categories){
+                const button = document.createElement("button")
+                button.innerText = categorie.name
+                filterButtons.appendChild(button)
+            }
+
+ 
+
+
     } catch (error) {
         console.error('Une erreur est survenue :', error);
     }
