7a4023c1db8b4900c01d85066181f2f731464e4d
Raphaël Grand <otumbarg@gmail.com>
2023-10-20 20:01:48 +0200

    filtre 1.0

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,92 +1,76 @@
+function displayWorks(works) {
+  const galleryDiv = document.getElementById("gallery");
+  galleryDiv.innerHTML = "";
 
-function displayWorks(works){
-    const galleryDiv = document.getElementById('gallery');
-    galleryDiv.innerHTML = ""
+  works.forEach((work) => {
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
 
-    works.forEach(work => {
-        const figure = document.createElement("figure");
-        const img = document.createElement("img");
-        const figcaption = document.createElement("figcaption");
+    img.src = work.imageUrl;
+    img.alt = work.title;
+    figcaption.innerText = work.title;
 
-        img.src = work.imageUrl;
-        img.alt = work.title;
-        figcaption.innerText = work.title;
-
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-        galleryDiv.appendChild(figure);
-    });
-
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    galleryDiv.appendChild(figure);
+  });
 }
 
-async function getWorks(){
-    const response = await fetch('http://localhost:5678/api/works');
-    const categoriesResponse = await fetch('http://localhost:5678/api/categories');
+async function getWorks() {
+  const response = await fetch("http://localhost:5678/api/works");
+  const categoriesResponse = await fetch(
+    "http://localhost:5678/api/categories"
+  );
 
-    if (!response.ok) {
-        throw new Error(response.statusText);
-    }
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
 
-        const works = await response.json();
-        displayWorks(works)
+  const works = await response.json();
 
-        const buttonAll = document.getElementById('0');
-        const buttonObject = document.getElementById('1');
-        const buttonHome = document.getElementById('2');
-        const buttonHotels = document.getElementById('3');
+  const categories = await categoriesResponse.json();
+  const buttonDiv = document.getElementById("filter-buttons");
+  displayWorks(works);
 
-        function event(button){
-            button.addEventListener('click', ()=>{
-                let test = works.filter((work)=>work.category.id = button.id)
-                displayWorks(test)
-             } )
+  const buttons = [];
+  console.log(works);
 
-        }
+  //creation boutons
+  for (let categorie of categories) {
+    const button = document.createElement("button");
+    button.id = categorie.id;
+    button.innerHTML = categorie.name;
+    buttonDiv.appendChild(button);
+    buttons.push(button);
+  }
 
-        buttonObject.addEventListener('click', ()=>{
-           let test = works.filter((work)=>work.category.id = buttonObject.id)
-           displayWorks(test)
-        } )
-    }
+  buttons[0].addEventListener("click", () => {
+    let filter = works.filter((work) => work.category.id == buttons[0].id);
+    displayWorks(filter);
+  });
 
-async function categories(){
-        const categoriesResponse = await fetch('http://localhost:5678/api/categories');
+  buttons[1].addEventListener("click", () => {
+    let filter = works.filter((work) => work.category.id == buttons[1].id);
+    displayWorks(filter);
+  });
 
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
+  buttons[2].addEventListener("click", () => {
+    let filter = works.filter((work) => work.category.id == buttons[2].id);
+    displayWorks(filter);
+  });
+}
 
-        const categories = await categoriesResponse.json();
-        const filterButtons = document.getElementById('filter-buttons');
-
-        function createButton(buttonName, buttonId){
-            const button = document.createElement("button")
-            button.innerText = buttonName
-            button.id = buttonId
-            filterButtons.appendChild(button)
-            }
-
-            createButton("Tous",0)
-        
-        for (const categorie of categories){
-            createButton(categorie.name, categorie.id)
-        }
-         
-    }
-
-   
-
-
-document.addEventListener('DOMContentLoaded', async function () {
-    try {  
-       
-
-     
-
-           
-    } catch (error) {
-        console.error('Une erreur est survenue :', error);
-    }
+document.addEventListener("DOMContentLoaded", async function () {
+  try {
+    getWorks();
+  } catch (error) {
+    console.error("Une erreur est survenue :", error);
+  }
 });
 
-/*Je recrupere */
+/*
+création bouton
+Addeventlistener
+
+ */
